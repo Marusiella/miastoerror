af255c95fc78ed381a5950710e900e4279efa1c4
Marusiella <67511608+Marusiella@users.noreply.github.com>
2022-10-11 17:57:32 +0200

    add zgloś button

--- lib/list_of.dart
+++ lib/list_of.dart
@@ -13,55 +13,57 @@ class ListHome extends StatelessWidget {
     var height = mediaQuery.size.height -
         mediaQuery.padding.top -
         mediaQuery.padding.bottom;
-    return Column(
-      children: [
-        SizedBox(
-          height: height * 0.07,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(
-                  onPressed: () => Navigator.pushNamed(context, "/add"),
-                  icon: const Icon(Icons.add)),
-              IconButton(
-                  onPressed: () =>
-                      Provider.of<MyProvider>(context, listen: false)
-                          .logOut(context),
-                  icon: const Icon(Icons.logout)),
-              Text(
-                upFirstLetter(Provider.of<MyProvider>(context).city),
-                style: TextStyle(
-                    fontSize: mediaQuery.size.height * 0.05,
-                    fontWeight: FontWeight.bold),
-              ),
-              IconButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text("Not implemented yet"),
-                    ));
-                  },
-                  icon: const Icon(Icons.settings)),
-            ],
+    return Container(
+      color: const Color.fromARGB(255, 33, 34, 35),
+      child: Column(
+        children: [
+          SizedBox(
+            height: height * 0.07,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () =>
+                        Provider.of<MyProvider>(context, listen: false)
+                            .logOut(context),
+                    icon: const Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                    )),
+                Text(
+                  upFirstLetter(Provider.of<MyProvider>(context).city),
+                  style: TextStyle(
+                      fontSize: mediaQuery.size.height * 0.05,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Roboto",
+                      color: Colors.white),
+                ),
+                IconButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text("Not implemented yet"),
+                      ));
+                    },
+                    icon: const Icon(
+                      Icons.settings,
+                      color: Colors.white,
+                    )),
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-            height: height * (1 - 0.2),
-            child: RefreshIndicator(
-              onRefresh: () async {
-                await Provider.of<MyProvider>(context, listen: false)
-                    .getPostNow();
-              },
-              child: ListView.builder(
-                itemCount: Provider.of<MyProvider>(context).posts.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: height * 0.2,
-                    child: Card(
-                      // border radius 10
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      color: Color.fromARGB(21, 255, 255, 255),
+          SizedBox(
+              height: height * (1 - 0.24),
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  await Provider.of<MyProvider>(context, listen: false)
+                      .getPostNow();
+                },
+                child: ListView.builder(
+                  itemCount: Provider.of<MyProvider>(context).posts.length,
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      height: height * 0.2,
                       child: Row(
                         children: [
                           Padding(
@@ -93,36 +95,54 @@ class ListHome extends StatelessWidget {
                                       .title,
                                   style: TextStyle(
                                       fontSize: mediaQuery.size.height * 0.03,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  ifTooLongShort(
-                                      Provider.of<MyProvider>(context)
-                                          .posts[index]
-                                          .description),
-                                  style: TextStyle(
-                                      fontSize: mediaQuery.size.height * 0.025,
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.black54),
+                                      color: Colors.white),
+                                ),
+                                SizedBox(
+                                  width: mediaQuery.size.width * 0.45,
+                                  child: Text(
+                                    ifTooLongShort(
+                                        Provider.of<MyProvider>(context)
+                                            .posts[index]
+                                            .description),
+                                    style: TextStyle(
+                                        fontSize:
+                                            mediaQuery.size.height * 0.025,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.white70),
+                                  ),
                                 ),
                               ],
                             ),
-                          )
+                          ),
                         ],
                       ),
-                    ),
-                  );
-                },
-              ),
-            ))
-      ],
+                    );
+                  },
+                ),
+              )),
+          TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.15,
+                      vertical: 20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15))),
+              onPressed: () => Navigator.pushNamed(context, "/add"),
+              child: const Text(
+                "Zgłoś",
+                style: TextStyle(color: Colors.black, fontSize: 17),
+              )),
+        ],
+      ),
     );
   }
 }
 
 String ifTooLongShort(String text) {
-  if (text.length > 20) {
-    return text.substring(0, 20) + "...";
+  if (text.length > 100) {
+    return "${text.substring(0, 20)}...";
   } else {
     return text;
   }
